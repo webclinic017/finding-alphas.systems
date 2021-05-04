b2f01333497fab0bf317fd0dca93c23af0a50603
DESKTOP-5PCUPEH\kim55 <karl6885@gmail.com>
2021-05-04 22:00:27 +0900

    requirement.txt 내용 변경 및 page config 변경

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import src.pages.home
 import src.pages.black_scholes_model
 import src.pages.square_test
 
+st.set_page_config(
+    page_title = "Finding Alphas",
+    page_icon = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/magnifying-glass-tilted-left_1f50d.png",
+)
+
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
