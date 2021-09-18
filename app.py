e66e51f682a4a474a7ac095446860e644d2274ba
DESKTOP-5PCUPEH\kim55 <karl6885@gmail.com>
2021-09-18 12:32:04 +0900

    Monte-Carlo Simulation for Price Prediction 구현

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import src.pages.introduction
 import src.pages.black_scholes_model
 import src.pages.stock_screener
+import src.pages.monte_carlo_simulation
 import src.pages.backtesting
 
 IMG_SRC = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/magnifying-glass-tilted-left_1f50d.png"
@@ -33,6 +34,7 @@ PAGES = {
     "Introduction": src.pages.introduction,
     "Black-Scholes Model": src.pages.black_scholes_model,
     "Stock Screener": src.pages.stock_screener,
+    "Monte-Carlo Simulation": src.pages.monte_carlo_simulation,
     "Backtesting": src.pages.backtesting,
 
 }
---
+++ src/pages/monte_carlo_simulation.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import FinanceDataReader as fdr
+
+def app():
+    st.title('Monte-Carlo Simulation for Price Prediction')
+
+    st.write('Simple Monte-Carlo simulation for price prediction with daily historical price data')
+
+    stock_dict = {
+        "(Stock) Apple": "AAPL",
+        "(Stock) Microsoft": "MSFT",
+        "(Stock) Alphabet": "GOOG",
+        "(Stock) Facebook": "FB",
+        "(Stock) Samsung Electronics": "005930",
+        "(Stock) SK Hynics": "000660",
+        "(Stock) Naver": "035420",
+        "(Stock) Kakao": "035720",
+        "(Crypto) BTC/USD": "BTC/USD",
+        "(Crypto) ETH/USD": "ETH/USD",
+        "(Crypto) XRP/USD": "XRP/USD",
+        "(FX) USD/EUR": "USD/EUR",
+        "(FX) USD/JPY": "USD/JPY",
+        "(FX) USD/KRW": "USD/KRW",
+    }
+
+    selected_stock_key = st.selectbox('Select price data to use in simulation', list(stock_dict.keys()))
+    selected_stock_value = stock_dict[selected_stock_key]
+
+    num_simulation = st.number_input("Number of simulations", value=1000)
+    num_days = st.number_input("Number of days to simulate", value=252)
+
+    if (st.button('Execute simulation')):
+        # 데이터 로드
+        price_df = fdr.DataReader(selected_stock_value, '2015-01-01')
+        last_price = price_df['Close'][-1]
+
+        simulation_df = pd.DataFrame()
+        # 몬테카를로 시뮬레이션 진행
+        for x in range(num_simulation):
+            count = 0
+            daily_vol = price_df['Change'].std()
+
+            price_series = []
+
+            price = last_price * (1 + np.random.normal(0, daily_vol))
+            price_series.append(price)
+
+            for y in range(num_days):
+                price = price_series[count] * (1 + np.random.normal(0, daily_vol))
+                price_series.append(price)
+                count += 1
+
+            simulation_df[x] = price_series
+
+        fig = plt.figure()
+        plt.suptitle('Monte Carlo Simulation for Price Prediction')
+        plt.title('Last price of '+selected_stock_key+' : '+str(last_price))
+        plt.plot(simulation_df)
+        plt.axhline(y=last_price, color='r', linestyle='-')
+        plt.xlabel('Day')
+        plt.ylabel('Price')
+        st.pyplot(fig)
